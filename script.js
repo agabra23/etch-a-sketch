1915c4f1e8fc7327b92bf9671cc98a8083519729
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-02-16 16:50:29 -0500

    Create setters and changeColor function

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const DEFAULT_COLOR = '#333333';
+const DEFAULT_MODE = 'color';
+const DEFAULT_SIZE = 16;
+
+let currentColor = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_SIZE;
+
+function setCurrentColor(newColor) {
+    currentColor = newColor;
+}
+
+function setCurrentSize(newSize) {
+    currentSize = newSize;
+}
+
+function setCurrentMode(newMode) {
+    currentMode = newMode;
+}
+
+
+
+
 function createGrid(rows) {
     var canvas = document.querySelector('.canvas');
     for (let i=0; i < rows; i++) {
@@ -5,6 +28,8 @@ function createGrid(rows) {
         row.classList.add('row');
         for (let x=1; x <=rows; x++) {
             let cell = document.createElement('div');
+            cell.addEventListener('mousedown', changeColor);
+            cell.addEventListener('mouseover', changeColor);
             cell.classList.add('cell');
             row.appendChild(cell);
         }
@@ -13,4 +38,14 @@ function createGrid(rows) {
     
 }
 
-createGrid(16);
+function changeColor(e) {
+    if (e.type === 'mouseover' && !'mousedown') {return}
+    if (currentMode === 'color') {
+        e.target.style.backgroundColor = currentColor;
+    } else if (currentMode === 'eraser') {
+        e.target.style.backgroundColor = '#fefefe'
+    }
+}
+
+
+window.onload = createGrid(DEFAULT_SIZE);
